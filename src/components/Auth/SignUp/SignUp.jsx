72d29ddbf99c09ae4d871e27bdf7ca0a22a83555
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-08-28 17:28:52 -0400

    finally fix it for real this time

--- src/components/Auth/SignUp/SignUp.jsx
+++ src/components/Auth/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { signUp } from "../../../../Services/auth";
+import { signUp } from "../../../../services/auth";
 
 import "./signup.css";
 const SignUp = ({ setUser }) => {
