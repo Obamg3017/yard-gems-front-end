ef0a83d0640e9c2a584be7fbda39908c821ef5f2
RF0510 <rosevelt.foushee@yahoo.com>
2024-08-30 12:28:45 -0400

    redoing landin

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,27 @@
-import React from "react"
-import './landing-page.css'
+import React from "react";
+import './landing-page.css';
+
 
 const LandingPage = () => {
+  const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", /* and so on for all 50 states */];
+
   return (
-    <div className="landing-page">
+    <div>
+
       <h1>Welcome to Our Application</h1>
+      <p>"Everyone on the internet is your neighbor."</p>
+      
+      <ul className="state-list">
+        {states.map(state => <li key={state}>{state}</li>)}
+      </ul>
+
+      
+
+      {/* Additional content can go here */}
+      <div className="additional-content">
+        <p>Explore more about our services.</p>
+        <button>Learn More</button>
+      </div>
     </div>
   );
 };
